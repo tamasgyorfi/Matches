f7a559b6e0f249468cf6573a6a4f9121fd12dad9
Tamas Gyorfi <gyorfitamas@gmail.com>
2017-07-01 13:55:26 +0100

    corrected akka bug

--- src/main/scala/hu/bets/matches/actors/SchedulesActor.scala
+++ src/main/scala/hu/bets/matches/actors/SchedulesActor.scala
@@ -3,6 +3,7 @@ package hu.bets.matches.actors
 import akka.actor.Actor
 import hu.bets.matches.dataaccess.SchedulesDao
 import hu.bets.matches.gateway.{MatchInfoGateway, ScheduleRetrievalException}
+import hu.bets.matches.model.ScheduledMatch
 import org.apache.log4j.Logger
 
 case class ScheduleRequest() {}
@@ -10,20 +11,31 @@ case class ScheduleRequest() {}
 class SchedulesActor(matchInfoGateway: MatchInfoGateway, schedulesDao: SchedulesDao) extends Actor {
 
   private val LOGGER: Logger = Logger.getLogger(classOf[SchedulesActor])
+  private val NR_OF_DAYS = 7
 
   override def receive: Receive = {
-    case _: ScheduleRequest => {
+    case ScheduleRequest =>
       try {
-        val scheduledMatches = matchInfoGateway getScheduledMatches 7
-        LOGGER info "Retrieved the following matches from the third party cervice: " + scheduledMatches
-
-        val failedSchedules = schedulesDao saveSchedules scheduledMatches
-        LOGGER info "The following schedules could not be saved into the database: " + failedSchedules
+        saveSchedules(getSchedules())
       } catch {
         case e: ScheduleRetrievalException => LOGGER error("Exception while trying to retrieve schedules. ", e)
         case ex: Exception => LOGGER error("Exception while trying to save schedules. ", ex)
       }
-    }
-    case message:Any => LOGGER info "Unknown message received: " + message
+
+    case message: Any => LOGGER info "Unknown message received: " + message
+  }
+
+  private def getSchedules(): List[ScheduledMatch] = {
+    val scheduledMatches = matchInfoGateway getScheduledMatches 7
+    LOGGER info "Retrieved the following matches from the third party cervice: " + scheduledMatches
+
+    scheduledMatches
+  }
+
+  private def saveSchedules(scheduledMatches: List[ScheduledMatch]) = {
+    val failedSchedules = schedulesDao.saveSchedules(scheduledMatches)
+    LOGGER info "The following schedules could not be saved into the database: " + failedSchedules
+
+    failedSchedules
   }
 }
--- src/test/scala/hu/bets/matches/actors/SchedulesActorTest.scala
+++ src/test/scala/hu/bets/matches/actors/SchedulesActorTest.scala
@@ -1,44 +1,45 @@
 package hu.bets.matches.actors
 
 import java.time.LocalDateTime
+import java.util.concurrent.TimeUnit
 
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import hu.bets.matches.dataaccess.SchedulesDao
 import hu.bets.matches.gateway.MatchInfoGateway
 import hu.bets.matches.model.{ScheduledMatch, Team}
+import org.junit.Test
 import org.mockito.Mockito._
 import org.scalatest.mockito.MockitoSugar
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
-class SchedulesActorTest extends TestKit(ActorSystem("Test")) with ImplicitSender
-  with WordSpecLike with Matchers with BeforeAndAfterAll with MockitoSugar {
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
-  override def afterAll {
-    TestKit.shutdownActorSystem(system)
-  }
+class SchedulesActorTest extends TestKit(ActorSystem("Test")) with ImplicitSender with MockitoSugar {
 
   private val matchInfoGateway = mock[MatchInfoGateway]
   private val scheduleDao = mock[SchedulesDao]
-
   private val sut = system.actorOf(Props(new SchedulesActor(matchInfoGateway, scheduleDao)))
 
-  "Schedules actor" must {
-    "do nothing when unknown message is received" in {
-      sut ! "Unknown Message"
+  @Test
+  def shouldDoNothingWhenUnknownMessageIsReceive(): Unit = {
+    sut ! "Unknown Message"
 
-      verifyZeroInteractions(matchInfoGateway)
-      verifyZeroInteractions(scheduleDao)
-    }
+    verifyZeroInteractions(matchInfoGateway)
+    verifyZeroInteractions(scheduleDao)
+  }
 
-    "retrieve and save schedules" in {
-      val matches = List(ScheduledMatch("sr:match:11854534", LocalDateTime.parse("2017-06-27T16:00"), "UEFA Champions League 17/18", Team("Alashkert", "ALA", "Armenia"), Team("FC Santa Coloma", "FCC", "Andorra")))
+  @Test
+  def shouldHandleScheduleRequestMessage(): Unit = {
 
-      when(matchInfoGateway.getScheduledMatches(7)).thenReturn(matches)
+    val matches = List(ScheduledMatch("sr:match:11854534", LocalDateTime.parse("2017-06-27T16:00"), "UEFA Champions League 17/18", Team("Alashkert", "ALA", "Armenia"), Team("FC Santa Coloma", "FCC", "Andorra")))
+    when(matchInfoGateway.getScheduledMatches(7)).thenReturn(matches)
+    when(scheduleDao.saveSchedules(matches)).thenReturn(List())
 
-      sut ! ScheduleRequest
+    sut ! ScheduleRequest
 
-      verify(scheduleDao).saveSchedules(matches)
-    }
+    TimeUnit.SECONDS.sleep(1)
+    verify(scheduleDao).saveSchedules(matches)
   }
 }
