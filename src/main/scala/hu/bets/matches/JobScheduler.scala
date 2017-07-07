19b70cd58c8a31e301651d78dfa40a8b5f8ec4ed
Tamas Gyorfi <gyorfitamas@gmail.com>
2017-07-07 10:20:40 +0100

    added scheduling of the scheduled matches

--- src/main/scala/hu/bets/matches/JobScheduler.scala
+++ src/main/scala/hu/bets/matches/JobScheduler.scala
@@ -1,13 +1,15 @@
 package hu.bets.matches
 
 import akka.actor.ActorRef
+import hu.bets.matches.actors.ScheduleRequest
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
-class JobScheduler(matchSchedulesActor: ActorRef) {
+class JobScheduler(schedulesActor: ActorRef, resultsActor: ActorRef) {
 
   def schedule(): Unit = {
-    AkkaSingletons.getScheduler.schedule(3 hours, 3 hours, null)
+    AkkaSingletons.getScheduler.schedule(0 hours, 3 hours, schedulesActor, ScheduleRequest())
+    AkkaSingletons.getScheduler.schedule(0 hours, 3 hours, null)
   }
 }
